c3aefb0b717637e2559cc58e0be746a1534913f7
Ilariiiiia <nicolamigone179@gmail.com>
2023-03-07 17:45:21 +0000

    initial commit

---
+++ pages/_header.tsx
@@ -0,0 +1,19 @@
+import headerStyles from "../styles/Header.module.css"
+
+export default function Header() {
+	return (
+		<>
+		<header className={headerStyles.header}>
+			<p>
+				Us
+			</p>
+			<p>
+				Data
+			</p>
+			<p>
+				Me
+			</p>
+		</header>
+		</>
+	)
+}
---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Header from './_header.tsx'
+
+const Home: NextPage = () => {
+	return (
+		<>
+		<Header>
+		</Header>
+		<footer></footer>
+		</>
+	)
+}
+
+export default Home
